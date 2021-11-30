9df00b0b8d99ae65574b57dba8d9fefd567618f0
tusharucd <94197099+tusharucd@users.noreply.github.com>
2021-11-30 11:17:57 +0000

    Add files via upload

---
+++ f.py
@@ -0,0 +1,346 @@
+from pandas.core.base import DataError
+from datetime  import date
+from datetime import timedelta
+from datetime import datetime
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+import yfinance as yf
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import streamlit as st
+import time
+# from scipy.stats import skew
+
+#Set the page content to cover entire screen
+st.set_page_config(layout="wide")
+
+#Function to load data according to selected stock, start date, end date
+@st.cache(allow_output_mutation=True)#Caching the data to improve performance
+def load_data(ticker):
+    data = yf.download(ticker, Start, Today)
+    data.reset_index(inplace = True)
+    return data
+
+#Function to load the title page
+def title():
+    st.title("Stock Predictor Application")
+    st.subheader("The Team")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("./Shatakshi.jpeg", width=200)
+        st.text("Shatakshi Brijpuriya\nStudent ID: 21200268")
+        
+    with col2:
+        st.image("./Yugandhara.jpeg",width=200)
+        st.text("Yugandhara Sawant\nStudent ID: 21200235")
+
+#Function to display company information
+def companyinfo(ticker):
+    stock = yf.Ticker(ticker)
+    info = stock.info
+    with st.spinner('Loading...'):
+        time.sleep(2)
+    st.subheader(info['longName']) 
+    st.markdown('** Sector **: ' + info['sector'])
+    st.markdown('** Industry **: ' + info['industry'])
+    st.markdown('** Phone **: ' + info['phone'])
+    st.markdown('** Address **: ' + info['address1'] + ', ' + info['city'] + ', ' + info['zip'] + ', '  +  info['country'])
+    st.markdown('** Business Summary **')
+    st.info(info['longBusinessSummary'])
+
+    fundamental = {
+            'Enterprise Value (USD)': info['enterpriseValue'],
+            'Enterprise To Revenue Ratio': info['enterpriseToRevenue'],
+            'Enterprise To Ebitda Ratio': info['enterpriseToEbitda'],
+            'Net Income (USD)': info['netIncomeToCommon'],
+            'Profit Margin Ratio': info['profitMargins'],
+            'Forward PE Ratio': info['forwardPE'],
+            'PEG Ratio': info['pegRatio'],
+            'Price to Book Ratio': info['priceToBook'],
+            'Forward EPS (USD)': info['forwardEps'],
+            'Beta ': info['beta'],
+            'Book Value (USD)': info['bookValue'],
+            'Dividend Rate (%)': info['dividendRate'], 
+            'Dividend Yield (%)': info['dividendYield'],
+            'Five year Avg Dividend Yield (%)': info['fiveYearAvgDividendYield'],
+            'Payout Ratio': info['payoutRatio']
+        }
+    
+    fund_info = pd.DataFrame.from_dict(fundamental, orient='index')
+    fund_info = fund_info.rename(columns={0: 'Value'})
+    st.subheader('Fundamental Info') 
+    st.table(fund_info)
+
+#Function to display data for last 5 days
+def tabular_data():
+    st.subheader("Stock data for last 5 days")
+    st.write(data.tail())
+
+#Function to display decriptive statistics on the stock data
+def descriptive_stats():
+    st.subheader("Statistical Analysis of Stock")
+    desc=data.describe()
+    desc.rename(index={
+        'count': 'Count', 
+        'mean': 'Mean', 
+        'std': 'Standard Deviation', 
+        'min': 'Minimum', 
+        '25%' : 'First Quartile', 
+        '50%' : 'Second Quartile', 
+        '75%' : 'Third Quartile', 
+        'max': 'Maximum'},
+        inplace =True)
+    st.write(desc)
+
+#Function to display time-series graph
+def plot_data():
+    st.subheader('Graphical Analysis of Stock')
+    fig = go.Figure()
+    fig.add_trace(
+            go.Scatter(
+                    x=data['Date'], 
+                    y=data['Open'], 
+                    name='stock open'
+                    )
+                )
+    fig.add_trace(
+            go.Scatter(
+                    x=data['Date'], 
+                    y=data['Close'], 
+                    name='stock close'
+                    )
+                )
+    fig.layout.update(title_text='Time Series Data',xaxis_title='Years', yaxis_title='Stock Price', xaxis_rangeslider_visible = True)
+    st.plotly_chart(fig,use_container_width=True)
+
+#Function to display candlestick graph
+def candlestick():
+    fig = go.Figure(
+        data=[go.Candlestick(
+            x=data['Date'],
+            open=data['Open'],
+            high=data['High'],
+            low=data['Low'] ,
+            close=data['Close'])])
+    fig.layout.update(
+        title_text='Candlestick data',
+        xaxis_rangeslider_visible = True,
+        xaxis_title = 'Years',
+        yaxis_title = 'Stock Price')
+    st.plotly_chart(fig,use_container_width=True)
+
+#Function to display Relative Strength Index on the stock
+def RSI():
+    st.subheader("Relative Strength Index (RSI)")
+    delta = data['Adj Close'].diff(1)
+    delta.dropna(inplace = True)
+
+    positive = delta.copy()
+    negative = delta.copy()
+    
+    positive[positive < 0] = 0
+    negative[negative > 0] = 0
+
+    period = st.number_input('Insert period (Days): ', min_value=1, max_value=100, value=14)
+    
+    avg_gain = positive.rolling(window = period).mean()
+    avg_loss = abs(negative.rolling(window = period).mean())
+
+    relative_strength = avg_gain / avg_loss
+    rsi = 100.0 - (100 / (1 + relative_strength))
+
+    rsi_data = pd.DataFrame()
+    rsi_data['Adj Close'] = data['Adj Close']
+    rsi_data['RSI'] = rsi
+    fig = go.Figure()
+    fig.add_trace(
+            go.Scatter(
+                    x=data['Date'], 
+                    y=rsi_data['RSI']
+                    )
+                )
+    fig.add_hline(y=70, line_dash='dash', line_color='green')
+    fig.add_hline(y=30, line_dash='dash', line_color='green')
+    fig.layout.update(
+        title_text='Relative Strength Index (RSI)',
+        xaxis_title='Years', 
+        yaxis_title='Percentage', 
+        xaxis_rangeslider_visible = True)
+    st.plotly_chart(fig,use_container_width=True) 
+
+#Function to graph Daily Returns on the stock
+def daily_returns():
+    close = data['Close']
+    returns = close.pct_change(1)
+    data['Returns'] = returns
+    st.subheader("Daily Returns")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Returns'], line = dict(color='firebrick')))
+    fig.update_layout(xaxis_title='Years', yaxis_title='Returns %', xaxis_rangeslider_visible = True)
+    st.plotly_chart(fig,use_container_width=True)
+
+#Function to calculate Moving Average on the stock
+def MovingAverage(data, size):
+    df = data.copy()
+    df['sma'] = df['Adj Close'].rolling(size).mean() #Simple Moving Average Array
+    df['ema'] = df['Adj Close'].ewm(span=size, min_periods=size).mean() #Exponential Moving Average Array
+    df.dropna(inplace=True)
+    return df
+
+#Function to display Moving Average on the stock
+def PlotMA(ticker):
+    st.subheader("Moving Average Analysis")
+    numYear = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=0)    
+    windowSize = st.number_input('Window Size (Day): ', min_value=5, max_value=500, value=20, key=1) 
+    start = date.today()-timedelta(numYear * 365)
+    end = date.today()
+    dataMA = yf.download(ticker,start,end)
+    df_ma = MovingAverage(dataMA, windowSize)
+    df_ma = df_ma.reset_index()
+    fig = go.Figure()
+    
+    fig.add_trace(
+            go.Scatter(
+                    x = df_ma['Date'],
+                    y = df_ma['Adj Close'],
+                    name = "Prices Over Last " + str(numYear) + " Year(s)"
+                )
+        )
+    
+    fig.add_trace(
+                go.Scatter(
+                        x = df_ma['Date'],
+                        y = df_ma['sma'],
+                        name = "SMA" + str(windowSize) + " Over Last " + str(numYear) + " Year(s)"
+                    )
+            )
+    
+    fig.add_trace(
+                go.Scatter(
+                        x = df_ma['Date'],
+                        y = df_ma['ema'],
+                        name = "EMA" + str(windowSize) + " Over Last " + str(numYear) + " Year(s)"
+                    )
+            )
+
+    fig.update_layout(legend_title_text='Trend', xaxis_rangeslider_visible = True)
+    fig.update_yaxes(tickprefix="$")
+    
+    st.plotly_chart(fig, use_container_width=True) 
+
+def descriptive():
+    close = data['Close']
+    returns = close.pct_change(1)
+    data['Returns'] = returns
+    st.subheader("Daily Return w.r.t mean and standard deviation")
+    mean = data['Returns'].mean()
+    std = data['Returns'].std()
+    fig = go.Figure()
+    fig.add_trace(
+        go.Histogram(x=returns)
+    )
+    fig.add_vline(x=mean, line_width=3, line_dash="dash", line_color="red")
+    fig.add_vline(x=std, line_width=3, line_dash="dash", line_color="green")
+    fig.add_vline(x=-std, line_width=3, line_dash="dash", line_color="green")
+    fig.update_layout(xaxis_title='Daily Returns')
+    st.plotly_chart(fig,use_container_width=True)
+
+def rolling_std():
+    close = data['Close']
+    returns = close.pct_change(1)
+    data['Returns'] = returns
+    st.subheader("20 days rolling standard deviation data")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=returns.rolling(20).std(), line = dict(color='blue')))
+    fig.update_layout(xaxis_title='20 days rolling standard deviation data')
+    st.plotly_chart(fig,use_container_width=True)
+
+def forecasting(ticker):
+    st.subheader('Select the modeling period')
+    start_train = st.date_input("Start date", date(2000, 1, 1), min_value=date(1980, 1, 1), max_value=date.today(), key='sd')
+    end_train = st.date_input("End date", date.today(), min_value=date(1980, 1, 1), max_value=date.today(), key ='ed')
+    if(end_train<=start_train):
+        st.error('Error: End date must fall after start date.')
+    else:
+        data_train = yf.download(ticker, start_train, end_train)
+        data_train.reset_index(inplace = True)
+        df_train = data_train[['Date','Close']]
+        df_train = df_train.rename(
+            columns={
+                'Date': 'ds',
+                'Close': 'y'
+                }
+        )
+
+    st.subheader('Select the prediction period')
+    n_years = st.slider("Years of prediction", 1, 10)
+    # end_date = st.date_input("End date", date.today(), min_value=date.today(), max_value=date(2100, 1, 1), key ='ed')
+
+    # st.write(data['Date'].dtypes)
+    # last_date = datetime.fromtimestamp(data['Date'].iloc[-1])
+    # diff = (date.today() - last_date).days
+    # period = (end_date - date.today()).days + diff
+    period = n_years * 365
+    with st.spinner('Loading...'):
+        m = Prophet()
+        m.fit(df_train)
+        future = m.make_future_dataframe(periods = period)
+        forecast = m.predict(future)
+
+        st.subheader("Forecast Data")
+        forecast_actual = forecast.copy()
+        forecast_actual = forecast_actual.rename(
+            columns={
+                'ds':'Date',
+                'yhat':'Predicted Stock Price',
+                'yhat_upper':'Predicted Upper Limit',
+                'yhat_lower':'Predicted Lower Limit'}
+                )
+        st.write(forecast_actual[['Date','Predicted Stock Price','Predicted Lower Limit','Predicted Upper Limit']].tail())
+
+        fig1 = plot_plotly(m,forecast)
+        fig1.update_layout(xaxis_title = 'Time', yaxis_title = 'Closing Price')
+        st.plotly_chart(fig1, use_container_width=True)
+
+        fig2 = m.plot_components(forecast)
+        st.write(fig2)
+
+
+if __name__ == "__main__":
+
+    companies = pd.read_csv("./nasdaq_screener_1636450584038.csv")
+    stocks = companies[['Symbol']]
+
+    with st.sidebar:
+        st.subheader("Enter Details")
+        selected_stock = st.selectbox("Select stock for prediction", stocks)
+        Start = st.date_input("Start date", date(2000, 1, 1), min_value=date(1980, 1, 1), max_value=datetime.today())
+        Today = st.date_input("End date", date.today(), min_value=date(1980, 1, 1), max_value=datetime.today())
+        if(Today<=Start):
+            st.error('Error: End date must fall after start date.')
+        menu = st.selectbox("Menu Options",('About us','Company Profile','Descriptive Data','Technical Indicators','Prediction')) 
+        data = load_data(selected_stock)
+
+    if(menu == "About us"):
+        title()
+    elif(menu == "Company Profile"):
+        companyinfo(selected_stock)
+    elif(menu == "Descriptive Data"):
+        tabular_data()
+        descriptive_stats()
+        descriptive()
+        rolling_std()
+    elif(menu == "Technical Indicators"):
+        plot_data() 
+        candlestick()
+        daily_returns()
+        PlotMA(selected_stock)
+        RSI()
+    elif(menu == "Prediction"):
+        forecasting(selected_stock)
+        
+
+        
